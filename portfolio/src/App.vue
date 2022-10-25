4a1a02177e13988fbd947951ac2051b02b6178c0
vlad <vpik0001@student.monash.edu>
2022-10-25 14:56:26 +0800

    added animations

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -1,36 +1,49 @@
 <template>
   <v-app>
     <v-main>
-      
-      <v-container fill-height fluid>
-        <v-row class="d-flex justify-center align-center">
+      <transition
+        mode="out-in"
+        leave-active-class="animate__animated animate__fadeOutUp"
+        v-if="projectNum==0">
+        <Introduction v-model="projectNum" />
+      </transition>
+      <transition
+        mode="out-in"
+        enter-active-class="animate__animated animate__fadeIn"
+        leave-active-class="animate__animated animate__fadeOutUp"
+        v-if="projectNum==1">
+        <Project1 v-model="projectNum"/>
+      </transition>
 
-            <div
-              class="animate__animated animate__backInUp d-flex text-h2 transition-swing mr-2"
-              v-text="'Vladislav'"
-            >
-            </div>
-
-            <div
-              class="animate__animated animate__backInDown d-flex text-h2 transition-swing ml-2"
-              v-text="'Pikulin'"
-            >
-            </div>
-        </v-row>
-      </v-container>
 
     </v-main>
   </v-app>
 </template>
-
 <script>
 import 'animate.css';
+import Introduction from './components/Introduction.vue'
+import Project1 from './components/Project1.vue'
+
 
 export default {
   name: 'App',
+  components:{
+    Introduction,
+    Project1
+  },
+  mounted() {
+    let icons1 = document.createElement('script')
+    icons1.setAttribute('src', "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js")
+    document.head.appendChild(icons1)
+
+    let icons2 = document.createElement('script')
+    icons2.setAttribute('src', "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js")
+    document.head.appendChild(icons2)
+  },
 
   data: () => ({
     screen_size: "lg", // xs,sm,md,lg,xl
+    projectNum: 0  // 0 = intro, 1 = first project, 2 = second project ... etc
   }),
 };
 </script>
